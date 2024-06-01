3441a57ae5d84e4b6dfa9c3ed2186310c844c8b2
Neil Doppler <neildoppler@Neils-Laptop.local>
2024-06-01 12:47:29 -0500

    added beginning functionality to the add Task function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,33 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
-let myModal = document.getElementById("myModal")
+let someId = JSON.parse(localStorage.getItem("nextId"));
+let myModal = document.getElementById("myModal");
+let taskNameInput = $('#taskTitle')
+let taskDueDateInput = $('#taskDueDate');
+let taskDescriptionInput = $('#taskDescription');
+let modalFormInput = document.getElementById("submit-btn")
 
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-if(!nextId) {
-    nextId = 1
-    return nextId
+if(!someId) {
+    someId = 1
+    return someId
 }
 else {
-    nextId++
-    return nextId
+    someId++
+    return someId
 }
 }
 
 // Todo: create a function to create a task card
 function createTaskCard() {
+
     generateTaskId();
 
     const taskCard = {
         taskName: 'taskName',
-        taskType: 'taskType',
+        taskDescription: 'taskDescription',
         taskDueDate: '05/31/2024'
     }
     taskList.push(taskCard);
@@ -40,10 +45,28 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    generateTaskId()
 
+    const taskName = taskNameInput.val().trim()
+    const taskDueDate = taskDueDateInput.val()
+    const taskDescription = taskDescriptionInput.val()
 
+    const task = {
+        taskId: someId,
+        title: taskName,
+        date: taskDueDate,
+        summary: taskDescription,
+        status: 'todo'
+    };
+    renderTaskList()
+    taskList.push(task);
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    localStorage.setItem("someId", JSON.stringify(someId));
+
+    taskNameInput.val('')
+    taskDueDateInput.val('')
+    taskDescriptionInput.val('')
 }
-
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
 
@@ -57,5 +80,8 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
     renderTaskList()
-
 });
+
+modalFormInput.addEventListener("submit", handleAddTask);
+
+modalForm
